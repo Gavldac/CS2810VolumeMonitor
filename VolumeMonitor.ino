d9180874625e2f118bd1588067d0d7b7fca60751
Gavldac <75696834+Gavldac@users.noreply.github.com>
2025-03-24 16:52:24 -0600

    Update VolumeMonitor.ino

--- VolumeMonitor.ino
+++ VolumeMonitor.ino
@@ -1,12 +1,18 @@
 #include <LiquidCrystal.h>
 
-const int vo = 3; // VO is the contrast of the LCD screen and can be controlled with PWM
-const int rs = A4;
-const int e = A5;
+const int vo = 3; // VO is the contrast of the LCD screen and can be controlled with PWM output
+const int rs = 13;
+const int e = 12;
 const int d4 = 2;
 const int d5 = 4;
 const int d6 = 7;
 const int d7 = 8;
+
+// Microphone will need to be grounded and use one of the Analog inputs. - suggested to use A0
+
+const int mic = A0;
+
+// Pins 9, 10, and 11 can be used for the LED signaling: 0-255 for birightness
 
 // int contrast = 0;
 
@@ -22,19 +28,19 @@ void setup()
 
   // Initialize the LCD with a size of 16x2 (columns x rows)
   lcd.begin(16, 2); //  16 columns and 2 rows on the display
-  lcd.setCursor(0, 0); // Set LCD cursor to top-left corner to write
 
   analogWrite(vo, 120); // sets the LCD contrast (0-255)
-
-  lcd.print("Hello, World!"); 
 }
 void loop()
 {
-  // contrast += 20;
-  // if (contrast > 255) contrast = 0;
-  // analogWrite(vo, contrast);// testing for adjusting LCD contrast 
+  // This just blinks the onboard Uno LED to show it's running the code
   digitalWrite(LED_BUILTIN, HIGH);
   delay(2000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
+
+  lcd.setCursor(0, 0); // Set LCD cursor to top-left corner to write
+  lcd.print("Put detected"); 
+  lcd.setCursor(0,1); // Move the cursor to the next row down
+  lcd.print("volume here");
 }
